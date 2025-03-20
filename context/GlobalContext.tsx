598680e77bcf43b4ba96d639e372c85c16d7183e
frankzeal33 <frankzeal33@gmail.com>
2025-03-20 08:05:47 +0100

    eslint updated

--- context/GlobalContext.tsx
+++ context/GlobalContext.tsx
@@ -4,12 +4,12 @@ import React, {
     useEffect,
     useState,
   } from "react";
-  const GlobalContext = createContext();
+  const GlobalContext = createContext(null);
   
   export const GlobalContextProvider = ({ children }: {children: any}) => {
   
     // input state
-    const [jobTitle, setJobTitle] = useState("");
+    const [jobTitle, setJobTitle] = useState(null);
 
   
     return (
