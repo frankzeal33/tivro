a8340ff1a9c8a12f1a05bdc919a000c85036c375
frankzeal33 <frankzeal33@gmail.com>
2025-07-04 03:16:16 +0100

    edited

--- app/flutterwave/verify/page.tsx
+++ app/flutterwave/verify/page.tsx
@@ -1,6 +1,7 @@
+"use client"
+
 export const dynamic = "force-dynamic"; // disables static rendering for this route
 
-"use client"
 import { axiosClient } from '@/GlobalApi'
 import { Loader2 } from 'lucide-react'
 import { useRouter, useSearchParams } from 'next/navigation'
