0e9cd69b414fd3db34a5a8c9396bbb51ae1ccbbf
frankzeal33 <frankzeal33@gmail.com>
2025-07-05 23:37:38 +0100

    edited

--- components/tenant-forms/FormCardFooter.tsx
+++ components/tenant-forms/FormCardFooter.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { CardFooter } from '../ui/card'
 import { Button } from '../ui/button'
 
-const FormCardFooter = ({text, loading, type="submit", handleClick}: {text: string; loading?: boolean; type: "submit" | "button"; handleClick?: () => void}) => {
+const FormCardFooter = ({text, loading, type="submit", handleClick}: {text: string; loading?: boolean; type?: "submit" | "button"; handleClick?: () => void}) => {
   return (
     <CardFooter className="p-4 border-t">
         <Button loading={loading} disabled={loading} type={type} className="w-full" onClick={handleClick}>
