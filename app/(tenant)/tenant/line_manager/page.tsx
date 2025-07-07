4796ae98e2145fa0be007c20b8f6e966fe2ad82b
frankzeal33 <frankzeal33@gmail.com>
2025-07-07 01:53:40 +0100

    added

--- app/(tenant)/tenant/line_manager/page.tsx
+++ app/(tenant)/tenant/line_manager/page.tsx
@@ -18,14 +18,8 @@ import { useParams, useRouter } from "next/navigation"
 import { FormEvent, useCallback, useEffect, useState } from "react"
 import { axiosClient } from "@/GlobalApi"
 import { toast } from "react-toastify"
-import { Loader2 } from "lucide-react"
-import { z } from "zod"
-import { debounce } from "lodash"
 import { useTenantStore } from "@/store/TenantStore"
 
-const otpSchema = z.object({
-  pin: z.string().min(6, "PIN must be 6 digits"),
-})
 
 const Page = () => {
 
@@ -33,72 +27,40 @@ const Page = () => {
   const params = useParams();
   const token = params.token as string;
   const [loading, setLoading] = useState(false)
-  const [pin, setPin] = useState("")
-  const [error, setError] = useState<string>("")
-  const [touched, setTouched] = useState(false)
 
   const tenantInfo = useTenantStore((state) => state.setTenantInfo)
 
    // Debounced validation
-    const validateOtp = useCallback(
-      debounce((value: string) => {
-        const result = otpSchema.safeParse({ pin: value })
-        if (!result.success) {
-          const message = result.error.formErrors.fieldErrors.pin?.[0] || "Invalid input"
-          setError(message)
-        } else {
-          setError("")
-        }
-      }, 300),
-      []
-    )
-
-    useEffect(() => {
-      if (touched) {
-        validateOtp(pin)
-      }
-    }, [pin, touched, validateOtp])
+   
 
   useEffect(() => {
-    if (!token) {
-      router.replace("/")
-    }
+    // if (!token) {
+    //   router.replace("/")
+    // }
   }, [token])
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault()
 
-    const result = otpSchema.safeParse({ pin })
-
-    if (!result.success) {
-      const message = result.error.formErrors.fieldErrors.pin?.[0] || "Invalid input"
-      setError(message)
-      toast.error(message)
-      setTouched(true)
-      return
-    }
-
-    setError("")
-
-    try {
+    // try {
     
-      setLoading(true)
+    //   setLoading(true)
       
-      const response = await axiosClient.get(`/tenant/?token=${token}&pin=${pin}`)
+    //   const response = await axiosClient.get(`/tenant/?token=${token}&pin=${pin}`)
 
-      if(response.data?.status === 500){
-        toast.error(response.data?.message)
-      }else{
-        tenantInfo(response.data)
-        toast.success("Verification Started")
-        router.push('/tenant/start-verification')
-      }
+    //   if(response.data?.status === 500){
+    //     toast.error(response.data?.message)
+    //   }else{
+    //     tenantInfo(response.data)
+    //     toast.success("Verification Started")
+    //     router.push('/tenant/start-verification')
+    //   }
 
-    } catch (error: any) {
-      toast.error(error.response?.data?.detail);
-    } finally {
-      setLoading(false)
-    } 
+    // } catch (error: any) {
+    //   toast.error(error.response?.data?.detail);
+    // } finally {
+    //   setLoading(false)
+    // } 
 
   }
   
@@ -112,32 +74,11 @@ const Page = () => {
               </Avatar>
               <CardTitle className="text-lg">Line Manager Verification</CardTitle>
               <CardDescription>
-                Enter verification code sent to your email address
+                Confirm that you are the line manager of this tenant.
               </CardDescription>
             </CardHeader>
             <form onSubmit={handleSubmit}>
-              <CardContent className="px-4 pb-4">
-                <div>
-                  <div className="grid gap-3">
-                    <div>
-                      <Input
-                        type="number"
-                        value={pin}
-                        onChange={(e) => setPin(e.target.value)}
-                        onBlur={() => setTouched(true)}
-                        placeholder="Enter pin"
-                        className="text-center"
-                      />
-                    </div>
-                    {touched && error && <p className="text-sm text-center text-red-500">{error}</p>}
-                    <div>
-                      <div className='w-full flex bg-muted gap-1 border border-l-primary border-l-6 rounded-md p-2'>
-                        <p className='text-muted-foreground text-xs md:text-sm'>Please help us confirm this email belongs to you</p>  
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </CardContent>
+              
               <CardFooter className="flex gap-5 p-4 border-t items-center justify-center">
                 <Button loading={loading} disabled={loading} type="submit">
                   {loading ? "Loading..." : "Yes"}
